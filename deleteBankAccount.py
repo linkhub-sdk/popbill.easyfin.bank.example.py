d60073ddb88038e2862ae2cae970a8c795cd804e
1jae <wjkim@linkhub.co.kr>
2021-04-29 15:38:19 +0900

    added deleteBankAccount API

---
+++ deleteBankAccount.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+import testValue
+from popbill import EasyFinBankService, PopbillException, BankAccountInfo
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
+
+easyFinBankService = EasyFinBankService(testValue.LinkID, testValue.SecretKey)
+easyFinBankService.IsTest = testValue.IsTest
+easyFinBankService.IPRestrictOnOff = testValue.IPRestrictOnOff
+easyFinBankService.UseStaticIP = testValue.UseStaticIP
+easyFinBankService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+'''
+종량제 이용시 등록된 계좌를 삭제합니다.
+- https://docs.popbill.com/easyfinbank/python/api#DeleteBankAccount
+'''
+
+try:
+    # 팝빌회원 사업자번호
+    CorpNum = settings.testCorpNum
+
+    # 팝빌회원 아이디
+    UserID = settings.testUserID
+
+    infoObj = BankAccountInfo(
+        # [필수] 은행코드
+        # 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+        # SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+        # 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+        BankCode="",
+
+        # [필수] 삭제할 계좌번호 하이픈('-') 제외
+        AccountNumber=""
+    )
+
+    response = easyFinBankService.deleteBankAccount(CorpNum, infoObj, UserID)
+
+    print("처리결과 : [%d] %s" % (result.code, result.message))
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
