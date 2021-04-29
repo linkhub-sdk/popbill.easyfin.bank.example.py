6526f48c1fd993375781fa3f1f3d9bd508105f3e
1jae <wjkim@linkhub.co.kr>
2021-04-29 15:32:03 +0900

    added getPaymentURL, getUseHistoryURL API

---
+++ getPaymentURL.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+import testValue
+
+from popbill import EasyFinBankService, PopbillException
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
+
+easyFinBankService = EasyFinBankService(testValue.LinkID, testValue.SecretKey)
+easyFinBankService.IsTest = testValue.IsTest
+easyFinBankService.IPRestrictOnOff = testValue.IPRestrictOnOff
+easyFinBankService.UseStaticIP = testValue.UseStaticIP
+easyFinBankService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+'''
+팝빌 연동회원 포인트 결재내역 URL을 반환합니다.
+- 보안정책에 따라 반환된 URL은 30초의 유효시간을 갖습니다.
+- https://docs.popbill.com/easyfinbank/python/api#GetPaymentURL
+'''
+
+try:
+    print("=" * 15 + " 연동회원 포인트 결재내역 URL 확인 " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 팝빌회원 아이디
+    UserID = testValue.testUserID
+
+    url = easyFinBankService.getPaymentURL(CorpNum, UserID)
+
+    print("URL: %s" % url)
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
